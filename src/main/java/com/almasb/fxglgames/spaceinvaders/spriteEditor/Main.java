8f30d0b8dad3f99a542976b4647b3d4a7c396b7a
James <james_morris1@mail.com>
2017-12-15 08:02:08 +0000

    Edit layout and add color picker

--- src/main/java/com/almasb/fxglgames/spaceinvaders/spriteEditor/Main.java
+++ src/main/java/com/almasb/fxglgames/spaceinvaders/spriteEditor/Main.java
@@ -1,25 +1,33 @@
 package com.almasb.fxglgames.spaceinvaders.spriteEditor;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
-	private final String title = "Sprite Editor v0.1";
+	private final String title = "Sprite Editor v0.2";
+	
+	protected Color colorPicker() {
+		
+		
+		return null;
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,790,580);
+			Scene scene = new Scene(root,550,610);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		    setUserAgentStylesheet(STYLESHEET_MODENA);
 			
 			Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("Layout.fxml"));
 			root.setCenter(content);
-			
+		    
 			primaryStage.setTitle(title);
 			primaryStage.setResizable(false);
 			
