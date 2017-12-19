7f30d5be445409fbbfaf2a0b10dfc477d2f36789
James Morris <james_morris1@mail.com>
2017-12-19 16:37:13 +0000

    Add color picker functionality and grid size options

--- src/main/java/com/almasb/fxglgames/spaceinvaders/spriteEditor/Controller.java
+++ src/main/java/com/almasb/fxglgames/spaceinvaders/spriteEditor/Controller.java
@@ -1,13 +1,60 @@
 package com.almasb.fxglgames.spaceinvaders.spriteEditor;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
 
 public class Controller {
 	
 	@FXML ColorPicker colorPicker;
-
-	public Controller() {
+	@FXML GridPane grid;
+	@FXML ComboBox<String> gridSize;
+	String style;
+	
+	public void initialize() {
+		ObservableList<String> gridOptions = FXCollections.observableArrayList(
+					"16x16 grid",
+					"32x32 grid",
+					"64x64 grid"
+				);
+		gridSize.setItems(gridOptions);
+		gridSize.getSelectionModel().selectFirst();
+	}
+	
+	@FXML public String getGridOption() {
 		
+		if ((String)(gridSize.getSelectionModel().getSelectedItem()) == "16x16 grid") {
+			System.out.println("16x16");
+		} else if ((String)(gridSize.getSelectionModel().getSelectedItem()) == "32x32 grid") {
+			System.out.println("32x32");
+		} else {
+			System.out.println("64x64");
+		}
+		
+		String selected = (String)(gridSize.getSelectionModel().getSelectedItem());
+		return selected;
+	}
+	
+	@FXML public void onButtonPress(ActionEvent event) {
+        Button x = (Button) event.getSource();
+		x.setStyle("-fx-background-color: "+colorPicker.getValue().toString().replace("0x", "#"));
+		style = x.getStyle();
+	}
+	
+	@FXML public void onMouseEnter(MouseEvent event) {
+		Button x = (Button) event.getSource();
+		style = x.getStyle();
+		x.setStyle("-fx-background-color: "+colorPicker.getValue().toString().replace("0x", "#"));
+	}
+	
+	@FXML public void onMouseExit(MouseEvent event) {
+		Button x = (Button) event.getSource();
+		x.setStyle(style);
 	}
 }
--- src/main/java/com/almasb/fxglgames/spaceinvaders/spriteEditor/Main.java
+++ src/main/java/com/almasb/fxglgames/spaceinvaders/spriteEditor/Main.java
@@ -2,32 +2,25 @@ package com.almasb.fxglgames.spaceinvaders.spriteEditor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	private final String title = "Sprite Editor v0.2.1";
-	
-	protected Color colorPicker() {
-		
-		
-		return null;
-	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,550,610);
+			Scene scene = new Scene(root, 550, 610);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		    setUserAgentStylesheet(STYLESHEET_MODENA);
 			
 			Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("Layout.fxml"));
 			root.setCenter(content);
-		    
+			
 			primaryStage.setTitle(title);
 			primaryStage.setResizable(false);
 			
